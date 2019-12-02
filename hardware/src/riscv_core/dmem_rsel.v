06f27cc05cf335124f94d5ff7f9e62e078c82cc7
Sue SU <xsu52@berkeley.edu>
2019-12-02 10:57:14 -0800

    dmem_rsel/dmem_wsel

--- hardware/src/riscv_core/dmem_rsel.v
+++ hardware/src/riscv_core/dmem_rsel.v
@@ -11,23 +11,26 @@ module dmem_rsel (
     input buttons_empty,
     input [2:0] buttons,
     input [1:0] switches,
-    input tx_ack
+    input tx_ack,
+	input note_finished
 );
 
     always @ (*) begin
-        case (addr[31:28])
-            4'b0100:
+        case (addr[31:30])
+            2'b01:
                 dout = bios_doutb;
-            4'b1000:
-                case (addr[5:4])
-                    2'b00:
-                        dout = addr[2] ? {24'b0, recv_data} : {30'b0, ~recv_empty, ~trmt_full};
-                    2'b10:
-                        dout = addr[3] ? {30'b0, switches} : addr[2] ? {29'b0, buttons} : {31'b0, buttons_empty};
-                    2'b11:
-                        dout = {31'b0, tx_ack};
+            2'b10:
+                case (addr[6:4])
+                    3'b000:
+                        dout = addr[2] ? {24'b0, recv_data} : {30'b0, ~recv_empty, ~trmt_full};   // uart_data: 32'h80000004, uart_control: 32'h80000000
+                    3'b001:
+						dout = addr[2] ? counter_inst : counter_cycle;   // cycle: 32'h80000010, inst: 32'h80000014
+					3'b010:
+                        dout = addr[3] ? {30'b0, switches} : addr[2] ? {29'b0, buttons} : {31'b0, buttons_empty}; //button_empty: 32'h80000020, button: 32'h80000024
+                    3'b100:																							// switches: 32'h80000028
+                        dout = {31'b0, tx_ack};							//ack: 32'h80000040
                     default:
-                        dout = addr[2] ? counter_inst : counter_cycle;
+                        dout = {31'b0, note_finished};						    // note_finished: 32'h8000100c
                 endcase
             default:
                 dout = dmem_douta;
--- hardware/src/riscv_core/dmem_wsel.v
+++ hardware/src/riscv_core/dmem_wsel.v
@@ -14,7 +14,18 @@ module dmem_wsel (
     output counter_reset,
     output leds_we,
     output tx_we,
-    output tx_duty_we
+    output tx_duty_we,
+	output sine_scale_we,
+	output square_scale_we,
+	output triangle_scale_we,
+	output sawtooth_scale_we,
+	output fcw_we,
+	output dac_source,
+	output gsr,
+	output note_start,
+	output note_release,
+	
+	
 );
 
     reg [3:0] wea;
@@ -69,9 +80,18 @@ module dmem_wsel (
 
     assign dmem_wea = addr[28] == 1'b1 ? wea : 4'b0000;
     assign imem_wea = addr[29] == 1'b1 && pc30 == 1'b1 ? wea : 4'b0000;
-    assign uart_we = addr[31] == 1'b1 && addr[5:3] == 3'b001 ? wea[0] : 1'b0;
-    assign counter_reset = addr[31] == 1'b1 && addr[5:3] == 3'b011 ? 1'b1 : 1'b0;
-    assign leds_we = addr[31] == 1'b1 && addr[5:3] == 3'b110 ? 1'b1 : 1'b0;
-    assign tx_we = addr[31] == 1'b1 && addr[5:4] == 2'b11 ? 1'b1 : 1'b0;    
-    assign tx_duty_we = addr[2];
+    assign uart_we = addr[31] == 1'b1 && addr[9] == 1'b0 && addr[3] == 1'b1 ? wea[0] : 1'b0;    //32'h80000008 
+    assign counter_reset = addr[31] == 1'b1 && addr[7:0] == 8'h18 ? |wea : 1'b0;    //32'h80000018 
+    assign leds_we = addr[31] == 1'b1 && addr[5:4] == 2'b11 && addr[3:2] == 2'b00? wea : 1'b0;   //32'h80000030
+    assign tx_duty_we = addr[31] == 1'b1 && addr[5:4] == 2'b11 && addr[2] == 1'b1 ? wea : 1'b0;   //32'h80000034
+    assign tx_we = addr[31] == 1'b1 && addr[5:3] == 3'b111 ? wea: 1'b0;   //32'h80000038
+	assign sine_scale_we = addr[31] == 1'b1 && addr[9] == 1'b1 && addr[3] == 1'b0 && addr[2] == 1'b0  ? wea: 1'b0;   //32'h80000200
+	assign square_scale_we = addr[31] == 1'b1 && addr[9] == 1'b1 && addr[2] == 1'b1 ? wea: 1'b0;   //32'h80000204
+	assign triangle_scale_we = addr[31] == 1'b1 && addr[9] == 1'b1 && addr[3] == 1'b1 ? wea: 1'b0;   //32'h80000208
+	assign sawtooth_scale_we = addr[31] == 1'b1 && addr[3:0] == 4'hc ? wea: 1'b0;   //32'h8000020c
+	assign fcw_we = addr[31] == 1'b1 && addr[12] == 1'b1 && addr[3] == 1'b0 && addr[2] == 1'b0 ? wea: 1'b0;   //32'h80001000
+	assign dac_source = addr[31] == 1'b1 && addr[6] == 1'b1 ? wea: 1'b0;   //32'h80000040
+	assign gsr = addr[31] == 1'b1 && addr[8] == 1'b1 ? |wea : 1'b0;   //32'h80000100
+	assign note_start = addr[31] == 1'b1 && addr[12] == 1'b1 && addr[2] == 1'b1 ? |wea: 1'b0;  //32'h80001004
+	assign note_release = addr[31] == 1'b1 && addr[12] == 1'b1 && addr[3] == 1'b1 ? |wea: 1'b0;  //32'h80001008
 endmodule
